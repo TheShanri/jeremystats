1bbb2386ef351a492a4157f496f1e30d86e88cfb
maliajaff <theexaminedexistence@gmail.com>
2025-09-18 13:54:42 -0400

    Update main.m

--- IED/main.m
+++ IED/main.m
@@ -10,6 +10,12 @@
 %SpikeAmpWidthScatter("C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_LLspikes_20250909_134703.xlsx","C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk.mat")
 %EventStacks_AmpWidth("C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_LLspikes_20250909_134703.xlsx","C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_uV.mat")
 EventStacks_AmpWidth_Avg("C:\Users\Z390\Desktop\IED DATA\Preped Data - M13s2aug1","C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk_uV.mat")
-%CSC2microVoltPatchCode("C:\Users\Z390\Desktop\IED DATA\LL_input_M13s2aug1_2023-08-01_12-11-26_mex_disk.mat")
-%%%next step... Run some test cases to see if averaging is happening right
+
+
+%%%next step... RN it is using first channel as an anchor, but doing wonky
+%%%stuff with the ampltitude placement. Trying to find a minmum... not sure
+%%%where in the code that's happening. 
+
+% Update code to take just the amp. and also wtf is going on with the
+% halfwidths?
 
