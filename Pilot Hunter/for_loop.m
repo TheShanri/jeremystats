35ba1d7eccb54e65f660e8fe30c66e2805b87d0c
maliajaff <theexaminedexistence@gmail.com>
2025-10-07 15:54:41 -0400

    .

--- Pilot Hunter/for_loop.m
+++ Pilot Hunter/for_loop.m
@@ -4,7 +4,7 @@
 %    - If your files are in the current working directory, use 'pwd'.
 %    - If they are in a specific folder, replace 'pwd' with the folder path
 %      (e.g., 'C:\MyData\2024\Exp1\').
-inputFolder = "D:\PTEN\PTEN\M13_pten\HF4s1aug1\2023-08-01_11-37-10";
+inputFolder = "D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26";
 
 % 2. Loop through files CS1.ncs to CS64.ncs
 for i = 1:3
@@ -51,20 +51,23 @@ fprintf('\n--- BATCH PROCESSING COMPLETE ---\n');
 
 % --- BATCH SCRIPT END ---
 
+--- BATCH PROCESSING COMPLETE ---
+for_loop
 
 --- Processing file 1 of 64: CSC1.ncs ---
-Reading data from D:\PTEN\PTEN\M13_pten\HF4s1aug1\2023-08-01_11-37-10\CSC1.ncs...
+Reading data from D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26\CSC1.ncs...
 	There was an error attempting to retrieve a string from index 0. Numeric data was found at that location.
 ❌ An error occurred while processing CSC1.ncs: One or more output arguments not assigned during call to "Nlx2MatCSC".
 
 --- Processing file 2 of 64: CSC2.ncs ---
-Reading data from D:\PTEN\PTEN\M13_pten\HF4s1aug1\2023-08-01_11-37-10\CSC2.ncs...
+Reading data from D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26\CSC2.ncs...
 	There was an error attempting to retrieve a string from index 0. Numeric data was found at that location.
 ❌ An error occurred while processing CSC2.ncs: One or more output arguments not assigned during call to "Nlx2MatCSC".
 
 --- Processing file 3 of 64: CSC3.ncs ---
-Reading data from D:\PTEN\PTEN\M13_pten\HF4s1aug1\2023-08-01_11-37-10\CSC3.ncs...
+Reading data from D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26\CSC3.ncs...
 	There was an error attempting to retrieve a string from index 0. Numeric data was found at that location.
 ❌ An error occurred while processing CSC3.ncs: One or more output arguments not assigned during call to "Nlx2MatCSC".
 
 --- BATCH PROCESSING COMPLETE ---
+
