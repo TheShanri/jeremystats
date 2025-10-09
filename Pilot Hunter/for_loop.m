a710a964705c7972e88e069fe0133cbaeea4f6c8
maliajaff <theexaminedexistence@gmail.com>
2025-10-09 17:42:04 -0400

    .

--- Pilot Hunter/for_loop.m
+++ Pilot Hunter/for_loop.m
@@ -4,10 +4,10 @@
 %    - If your files are in the current working directory, use 'pwd'.
 %    - If they are in a specific folder, replace 'pwd' with the folder path
 %      (e.g., 'C:\MyData\2024\Exp1\').
-inputFolder = "D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26";
+inputFolder = 'D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26';
 
 % 2. Loop through files CS1.ncs to CS64.ncs
-for i = 1:3
+for i = 1:
     % Construct the current file name (e.g., 'CS1.ncs', 'CS2.ncs', etc.)
     currentFileName = sprintf('CSC%d.ncs', i);
     
@@ -29,8 +29,8 @@ for i = 1:3
     fprintf('Reading data from %s...\n', ncsFilePath);
     
     try
-        [Timestamps, ScNumbers, SampleFrequencies, NumberOfValidSamples, Samples, Header] = ...
-            Nlx2MatCSC(ncsFilePath, [1 1 1 1 1], 1, 1, []);
+        [Timestamps, ScNumbers, SampleFrequencies, NumberOfValidSamples, Samples] = ...
+            Nlx2MatCSC(ncsFilePath, [1 1 1 1 1], 0, 1, []);
         
         % 5. Save the retrieved data to the specified .mat file path.
         fprintf('Saving data to %s...\n', matFilePath);
@@ -50,24 +50,4 @@ end
 fprintf('\n--- BATCH PROCESSING COMPLETE ---\n');
 
 % --- BATCH SCRIPT END ---
-
---- BATCH PROCESSING COMPLETE ---
-for_loop
-
---- Processing file 1 of 64: CSC1.ncs ---
-Reading data from D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26\CSC1.ncs...
-	There was an error attempting to retrieve a string from index 0. Numeric data was found at that location.
-❌ An error occurred while processing CSC1.ncs: One or more output arguments not assigned during call to "Nlx2MatCSC".
-
---- Processing file 2 of 64: CSC2.ncs ---
-Reading data from D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26\CSC2.ncs...
-	There was an error attempting to retrieve a string from index 0. Numeric data was found at that location.
-❌ An error occurred while processing CSC2.ncs: One or more output arguments not assigned during call to "Nlx2MatCSC".
-
---- Processing file 3 of 64: CSC3.ncs ---
-Reading data from D:\PTEN\PTEN\M13_pten\HF4s2aug1\2023-08-01_12-11-26\CSC3.ncs...
-	There was an error attempting to retrieve a string from index 0. Numeric data was found at that location.
-❌ An error occurred while processing CSC3.ncs: One or more output arguments not assigned during call to "Nlx2MatCSC".
-
---- BATCH PROCESSING COMPLETE ---
 
